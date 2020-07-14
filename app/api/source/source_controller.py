bf03f12951d639250c00a52c78319948c4b52981
Marco Zingales <marcozingales@marcos-mbp.home>
2020-07-14 12:17:51 +0200

    feat: removed comments and changed post request file read

--- app/api/source/source_controller.py
+++ app/api/source/source_controller.py
@@ -3,10 +3,10 @@ from flask import request, jsonify
 from app.config_dev import MONGODB_DB, MONGODB_METADATA_COLLECTION
 from app import db
 from app import utils
-# from app.utils import values_not_found, wrong_columns_set, def_columns_present
 from app.constants import INVALID_XML, MISSING_FILE_KEY, MISSING_FILE_NAME
 from app.exceptions import CustomError
 from io import TextIOWrapper
+
 
 def upload_file():
     """
@@ -20,36 +20,27 @@ def upload_file():
     """
 
     db_client = db.mongo.cx
-    dp_coll = db_client[MONGODB_DB][MONGODB_METADATA_COLLECTION]
-
     args = request.args.to_dict()
     object_type = args['object_type']
-    metadata_collection= db_client[MONGODB_DB][MONGODB_METADATA_COLLECTION]
+    object_type.lower()
+    metadata_collection = db_client[MONGODB_DB][MONGODB_METADATA_COLLECTION]
 
-    if 'file' not in request.files:
+    if 'file' not in request.form:
         raise CustomError(MISSING_FILE_KEY)
-    file = request.files['file']
-    if file.filename == '':
+    file = request.form['file']
+
+    if request.form['filename'] == '':
         raise CustomError(MISSING_FILE_NAME)
-    file_extension_ok = utils.is_file_extension_allowed(file.filename)
+    file_extension_ok = utils.is_file_extension_allowed(request.form['filename'])
     if not file_extension_ok:
         raise CustomeError(INCORRECT_FILE_TYPE)
-    
-    fstring = file.read().decode("utf-8")
-    #fstring = file.read().decode("utf-8").encode('ascii')
-    
-    #xml_valid = utils.is_valid_xml(file, object_type)
-    file.seek(0)
-    #xml_valid = utils.is_valid_xml(file.read(), object_type)
-    #xml_valid = utils.is_valid_xml(TextIOWrapper(file.read()), object_type)
+
+    fstring = file
     xml_valid = utils.is_valid_xml(fstring.encode(), object_type)
     # TODO: turn the file to XML format, check validity and split to xml nodes, instead of each line being one record
-    
-    # referenced_object_registered = utils.is_referenced_object_registered(fstring, object_type, 'metadata')
-    # if referenced_object_registered:
+
     if xml_valid:
-        utils.collection_writer(fstring, object_type,'metadatadb', dp_coll)
+        utils.collection_writer(fstring, object_type, 'metadatadb', metadata_collection)
         return jsonify({"message": "uploading data.."}), 200
     else:
         raise CustomError(INVALID_XML)
-
