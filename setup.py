714b7201d0ebb73cadaf063ed7ff4447bf75596e
Marco Zingales <marcozingales@marcos-mbp.home>
2020-10-09 11:20:21 +0200

    refreshed ignored files.

--- setup.py
+++ setup.py
@@ -6,19 +6,15 @@ with open('requirements.txt') as f:
 with open("requirements-build.txt") as f:
     build_requirements = f.read().splitlines()
 
-with open("runtime/requirements-deploy.txt") as f:
-    deploy_requirements = f.read().splitlines()
-
-
 setup(
-    name='query_api',
+    name='sda-metadata-submission-api',
     version='1.0.0',
     long_description=__doc__,
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     install_requires=requirements,
-    extras_require={
-        "dev": build_requirements + deploy_requirements,
-    },
+    # extras_require={
+    #     "dev": build_requirements + deploy_requirements,
+    # },
 )
